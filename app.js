c64cd5802a076de8624fe2ffcdd11b0cdf6e645b
Brian Flores <brian.flores.ch@gmail.com>
2022-03-15 23:20:44 -0300

    Linkeando vistas

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const port = 4000;
 app.use(express.static(path.join(__dirname, 'public')))
 
 /* routes */
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html/'))})
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/home.html/')))
+app.get('/Login.html', (req, res) => res.sendFile(path.join(__dirname, '/views/Login.html/')))
+app.get('/register.html', (req, res) => res.sendFile(path.join(__dirname, '/views/register.html/')))
+app.get('/productCard.html', (req, res) => res.sendFile(path.join(__dirname, '/views/productCard.html/')))
 
 app.listen(port, () => console.log(`Servidor levantado en el puerto ${port}
 http://localhost:${port}`))
