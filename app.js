12e606937e06df7ff85fd8f4703ed806f9114a2e
Brian Flores <brian.flores.ch@gmail.com>
2022-03-27 23:19:49 -0300

    Mejora ruteo

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 /* routes */
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/home.html/')))
-app.get('/Login.html', (req, res) => res.sendFile(path.join(__dirname, '/views/Login.html/')))
-app.get('/register.html', (req, res) => res.sendFile(path.join(__dirname, '/views/register.html/')))
-app.get('/productCard.html', (req, res) => res.sendFile(path.join(__dirname, '/views/productCard.html/')))
-app.get('/detalle.html', (req, res) => res.sendFile(path.join(__dirname, '/views/detalle.html/')))
+app.get('/Login', (req, res) => res.sendFile(path.join(__dirname, '/views/Login.html/')))
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, '/views/register.html/')))
+app.get('/productCard', (req, res) => res.sendFile(path.join(__dirname, '/views/productCard.html/')))
+app.get('/detalle', (req, res) => res.sendFile(path.join(__dirname, '/views/detalle.html/')))
 
 app.listen(port, () => console.log(`Servidor levantado en el puerto ${port}
 http://localhost:${port}`))
