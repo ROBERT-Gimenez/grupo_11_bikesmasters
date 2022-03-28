76117d488fa96b480172ed6fbcc48a854c4f272b
carlosmoreno <carlosmorenosanabria77@gmail.com>
2022-03-28 18:43:22 -0300

    carrito de compras modificado

---
+++ public/js/carrito.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    update_amounts();
+$('.qty, .pice').on('keyup keypress blur change',function(e){
+    update_amounts();
+});
+});
+function update_amounts(){
+    var sum = 0.0;
+    $('#myTable > tbody > tr').each (function(){
+        var qty = $(this).find ('.qty').val();
+        var price = $(this).find ('.price').val()
+        var amount = (qty*price)
+        sum+=amount;
+        $(this).find('.amount').text(''+amount);
+    });
+    $('.total').text(sum);
+}
+
+//incrementar o bajar cantidad//
+var incrementQty;
+var decrementQty;
+var plusBtn = $(".cart-qty-plus");
+var minusBtn = $(".cart-qty-minus");
+var incrementQty = plusBtn.click (function() {
+    var $n = $(this)
+    .parent(".button-container")
+    .find(".qty");
+    $n.val(Number($n.val())+1 );
+    update_amounts();
+});
+var decrementQty = minusBtn.click(function(){
+    var $n = $(this)
+    .parent(".button-container")
+    .find(".qty")
+    var Qtyval = Number($n.val());
+    if (Qtyval> 0){
+        $n.val(Qtyval-1);
+    }
+    update_amounts();
+});
