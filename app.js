3b750331fb9b96c7b083024d05cb62710092d30b
robert giemenz <you@example.com>
2022-04-04 22:47:56 -0300

    config ejs

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express()
 const path = require('path')
 const port = 4000;
 
-app.set('view engine' , 'ejs');
+/* Views config */
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, 'public')))
 
 /* routes */
