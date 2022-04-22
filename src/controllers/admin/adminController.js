4bc25e537babe4c4c2f001cd1a26c2fb991a0795
Brian Flores <brian.flores.ch@gmail.com>
2022-04-22 00:01:04 -0300

    Agrego campo en addProduct y actualizo json de productos

--- src/controllers/admin/adminController.js
+++ src/controllers/admin/adminController.js
@@ -32,14 +32,15 @@ module.exports = {
 
         let newProduct = {
             id: lastId + 1,
-            image: req.file ? req.file.filename : "products/product-default-2.png",
+            image: req.file ? req.file.filename : "products/product-default-4.png",
             name: req.body.name,
             price: req.body.price,
             marca: req.body.marca,
             description: req.body.description,
             stock: req.body.stock,
             discount: req.body.discount,
-            categoryId: req.body.categoryId
+            categoryId: req.body.categoryId,
+            condition: req.body.condition
         }
         
         // Paso 2 - Guardar el nuevo producto en el array de usuarios
--- src/middlewares/imageProductMiddleware.js
+++ src/middlewares/imageProductMiddleware.js
@@ -6,7 +6,7 @@ const storage = multer.diskStorage({
         cb(null, path.join(__dirname, '../../public/images/products'))
     },
     filename: (req, file, cb) => {
-        let newFile = `${Date.now()}-product-${path.extname(file.originalname)}`;
+        let newFile = `${Date.now()}-product${path.extname(file.originalname)}`;
         cb(null, newFile)
     }
 })
