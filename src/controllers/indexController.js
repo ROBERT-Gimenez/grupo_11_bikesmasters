1d9e94355b26e5665869a270a37b6ff6ff10c691
Brian Flores <brian.flores.ch@gmail.com>
2022-05-05 23:06:46 -0300

    Mejora estilos de register y admin

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,9 +1,3 @@
-const fs = require('fs');
-const path = require('path');
-
-/* const productsFilePath = path.join(__dirname, '../data/products.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); */
-
 const {users , writeUsers, products} = require('../data/index');
 const { validationResult } = require('express-validator');
 
