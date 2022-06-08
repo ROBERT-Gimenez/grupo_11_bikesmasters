0f823ae546182c08b9142112344cf4aaba024691
carlosmoreno <carlosmorenosanabria77@gmail.com>
2022-06-08 09:51:33 -0300

    categoria

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,5 @@
 /* const {products , categories} = require('../data/index');*/
-const categorias = require('../data/categories')
+/*const categorias = require('../data/categories')*/
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const db =require('../database/models');
 
@@ -23,34 +23,44 @@ module.exports = {
 	
 	},
 	Category: (req, res) => {
-		let namecategori = categorias.find(categori => categori.id === +req.params.id)
+		/*let namecategori = categorias.find(categori => categori.id === +req.params.id)
 		let Categori = [];
             products.forEach(product => {
                 if(product.categoryId === +req.params.id){
                     Categori.push(product)
                 }
             }
-            );
-            res.render ('products/Categorias',{
-                titulo: 'Bikesmasters',
-                css: 'home.css',
-                Categori: Categori ,
-                toThousand,
-                session:req.session,
-				namecategori
-
+            );*/
+            db.categoria.findByPk(+req.params.id)
+            .then((categorias)=>{
+                res.render ('products/Categorias',{
+                    titulo: 'Bikesmasters',
+                    css: 'home.css',
+                    Categori: Categori ,
+                    toThousand,
+                    session:req.session,
+                    categorias
+    
+                })
             })
+            .catch((error)=> res.send(error))
 	
 	},
 	Categoryadmin: (req, res) => {
-		let namecategori = categorias.find(categori => categori.id === +req.params.id)
+        db.categoria.create({
+            nombre: req.body.nombre
+        }).then((result)=>{
+            res.redirect("productController.Categoryadmin")
+        })
+
+		/*let namecategori = categorias.find(categori => categori.id === +req.params.id)
 		let Categori = [];
             products.forEach(product => {
                 if(product.categoryId === +req.params.id){
                     Categori.push(product)
                 }
             }
-            );
+            );*/
             res.render ('Admin/Categoriadmin',{
                 titulo: 'Bikesmasters',
                 css: 'adminIndex.css',
