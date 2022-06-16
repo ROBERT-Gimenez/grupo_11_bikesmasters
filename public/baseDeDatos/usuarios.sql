6de1a9f64a083bfa6e81334d6431b4e289e7f2b6
robert giemenz <you@example.com>
2022-06-15 23:35:22 -0300

    script de datos subido

---
+++ public/baseDeDatos/usuarios.sql
@@ -0,0 +1,9 @@
+INSERT INTO bikemasterg11.usuarios (name,email,password,telefono,direccion_id,avatar,rol_id) VALUES
+	 ('robert','mail@mail.com','123456789','1141806012',1,NULL,2),
+	 ('roberto','prueba@prueba.com','$2a$10$4CgdnwEsQj4Tv',NULL,NULL,'user-default.png',1),
+	 ('pepito','pepito@pepito.com','$2a$10$hjmz8Fwqud9Kz',NULL,NULL,'user-default.png',1),
+	 ('briana','brian@brian.com','$2a$10$3e7TribuwU4ej','1145698714',NULL,'user-default.png',1),
+	 ('betty','bety@bety.com','$2a$10$0gWO.o8LnVKM2','1212121212',3,'1655326035506-profile.jpg',2),
+	 ('donald','mac@donald.com','$2a$10$D7j4tK/qeAdAq',NULL,NULL,'user-default.png',1),
+	 ('loki','loki@loki.com','$2a$10$3zYEn2vj29sBX','12346789',2,'1654825760980-profile.jpg',1),
+	 ('pato','pato@pato.com','$2a$10$sx/t8dVmd.g1X','15471715',NULL,'1654823699746-profile.jpg',1);
