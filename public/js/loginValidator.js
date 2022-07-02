6505cbc3589d19ec0795593a30fb6815224ac4a7
Brian Flores <brian.flores.ch@gmail.com>
2022-07-01 23:36:53 -0300

    Agrega editProfileValidation.js para el front

--- public/js/loginValidator.js
+++ public/js/loginValidator.js
@@ -9,7 +9,7 @@ const qsAll = (element) => {
 window.addEventListener('load', () => {
 
     regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
-        regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,12}$/;
+    regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,12}$/;
 
     let $email = qs('#email')
     let $emailError = qs('#error_email')
