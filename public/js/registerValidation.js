8637206f73903f1af45b8fb9579f6da6e6483a39
Brian Flores <brian.flores.ch@gmail.com>
2022-07-26 00:05:40 -0300

    Modifica estilos de register

--- public/js/registerValidation.js
+++ public/js/registerValidation.js
@@ -53,37 +53,37 @@ function checkChar(e) {
     }
 }
 
-const validInput = (icon)=>{
+/* const validInput = (icon)=>{
     icon.classList.remove("input-error")
     icon.classList.add("input-pass")
     icon.classList.remove("fa-circle-exclamation","icon__invalid")
     icon.classList.add("fa-solid", "fa-circle-check","icon__valid")
-}
+} */
 
-const invalidInput = (icon)=>{
+/* const invalidInput = (icon)=>{
     icon.classList.remove("input-pass")
     icon.classList.add("input-error")
     icon.classList.remove("fa-circle-check","icon__valid")
     icon.classList.add("fa-solid","fa-circle-exclamation","icon__invalid")
     
-}
+} */
 
 $name.addEventListener('blur' , function(e){
         switch (true) { 
             case !$name.value.trim()||$name.value.length<4:
                 $nameError.innerHTML= "Se requiere un Nombre valido"
                 $name.classList.add("invalido")
-                invalidInput($name.nextElementSibling)
+/*                 invalidInput($name.nextElementSibling) */
                 break;
             case !regExAlpha.test($name.value):
                 $nameError.innerHTML ="Nombre Invalido , no debe poseer numeros"
                 $name.classList.add("invalido");
-                invalidInput($name.nextElementSibling)
+/*                 invalidInput($name.nextElementSibling) */
                 break;
             default :
                 $name.classList.remove("invalido");
                 $name.classList.add("valido");
-                validInput($name.nextElementSibling)
+/*                 validInput($name.nextElementSibling) */
                 $nameError.innerHTML =""
                 break;
             }
@@ -95,18 +95,18 @@ $email.addEventListener("blur" , function(e) {
         case !$email.value.trim():
             $emailError.innerHTML= "Email Requerido";
             $email.classList.add("invalido")
-            invalidInput($email.nextElementSibling)
+/*             invalidInput($email.nextElementSibling) */
             break;
         case !regExEmail.test($email.value):
             $emailError.innerHTML ="Email Invalido"
             $email.classList.add("invalido");
-            invalidInput($email.nextElementSibling)
+/*             invalidInput($email.nextElementSibling) */
             break;
         default :
             $email.classList.remove("invalido");
             $email.classList.add("valido");
             $emailError.innerHTML =""
-            validInput($email.nextElementSibling)
+/*             validInput($email.nextElementSibling) */
             break;
         }
 })
@@ -116,7 +116,7 @@ $email.addEventListener('blur', () => {
     .then(result => {
         result.data.forEach(element => {
             if($email.value === element.email){
-                invalidInput($email.nextElementSibling)
+/*                 invalidInput($email.nextElementSibling) */
                 $emailError.innerHTML = "email ya registrado";
                 $email.classList.add('invalido');
             }
@@ -129,17 +129,17 @@ $password.addEventListener('blur', function(){
         case !$password.value.trim():
             $passwordError.innerHTML = 'El campo contraseña es obligatorio'
             $password.classList.add('invalido')
-            invalidInput($password.nextElementSibling)
+/*             invalidInput($password.nextElementSibling) */
             break;
         case !regExPass.test($password.value):
             $passwordError.innerHTML = 'La contraseña debe tener: entre 8 y 12 caracteres, al menos una mayúscula, una minúscula y un número';
             $password.classList.add('invalido')
-            invalidInput($password.nextElementSibling)
+/*             invalidInput($password.nextElementSibling) */
             break;    
         default:
             $password.classList.remove("invalido");
             $password.classList.add('valido')
-            validInput($password.nextElementSibling)
+/*             validInput($password.nextElementSibling) */
             $passwordError.innerHTML = ""
             break;
     }
@@ -150,8 +150,8 @@ $password2.addEventListener('blur', function(){
         case $password2.value.length == "":
             $password2Error.innerHTML = 'Reingresa tu contraseña'
             $password2.classList.add('invalido')
-            invalidInput($password2.nextElementSibling)
-            break;
+/*             invalidInput($password2.nextElementSibling)
+ */            break;
         case $password2.value !== $password.value:
             $password2Error.innerHTML = 'Las contraseñas no coinciden';
             $password2.classList.add('invalido')
@@ -160,8 +160,8 @@ $password2.addEventListener('blur', function(){
         default:
             $password2.classList.remove("invalido");
             $password2.classList.add('valido')
-            validInput($password2.nextElementSibling)
-            $password2Error.innerHTML = ""
+/*             validInput($password2.nextElementSibling)
+ */            $password2Error.innerHTML = ""
             break;
     }
 })
@@ -198,8 +198,8 @@ form.addEventListener("submit", function (event) {
         || elementsForm[i].classList.contains('invalido')) 
         {
             elementsForm[i].classList.add('invalido')
-            invalidInput(elementsForm[i].nextElementSibling)
-            submitError.innerHTML = "Verifique los datos"
+/*             invalidInput(elementsForm[i].nextElementSibling)
+ */            submitError.innerHTML = "Verifique los datos"
             submitError.style.color="red"
             errors = true
         }else{elementsForm[i].style.boxShadow = "none"
