c4cd128a587c6a9a87fbfbe080a37527d81462ac
robert giemenz <you@example.com>
2022-07-27 04:49:55 -0300

    correccion de formula del carrito

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -70,11 +70,13 @@ let btnDelete = document.querySelectorAll('button.delete');
 let ProdPrice = document.querySelectorAll('p.subtotal')
 let AllPrices = document.querySelector('b#subTotal')
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-let carro = []
-JSON.parse(carrito).forEach(item =>{
+
+
+    let carro = []
+    JSON.parse(carrito).forEach(item =>{
 
 /* --------------Productos a Mostrar---------------------------- */
-productos.forEach(product=>{
+    productos.forEach(product=>{
     let product_id = product.getAttribute("target");
 
         if(item== product_id){
@@ -83,9 +85,9 @@ productos.forEach(product=>{
             }
         })
     
-/* ------------------Precio------------------------- */
+/* ---------------------------Precio--------------------------- */
         
-        ProdPrice.forEach(t => {
+    ProdPrice.forEach(t => {
                
         if(item==(t.getAttribute("target"))){
             carro.push(t.textContent)
@@ -95,66 +97,65 @@ productos.forEach(product=>{
            AllPrices.innerHTML=toThousand(carro.reduce((a,b) => {return Number(+a - -b) }))
             }    
         })
-    
      
  /* --------------Precio al Cambiar las Cantidades--------------- */
- productos.forEach((product)=>{
-    let price = Number(product.children[1].getAttribute("target"))
-    let select = product.children[2];
-    let Total = product.children[3];
-    Total.innerHTML= toThousand(price)
-    if(product.style.display !== "none"){
-/* --------------SI CAMBIA EL PRODUCTO------------------------ */
-     product.onchange = function(event) {
-    
-    Total.innerHTML = toThousand(+select.value * price)
-    carro=[]
-    ProdPrice.forEach(t => {
-        
-        if(carrito.includes(t.getAttribute("target"))){
-            carro.push((t.textContent).replace("." , ""))    
-            }
-        })
-        console.log(carro) 
-        AllPrices.innerHTML=toThousand(carro.reduce((a,b) => {return Number(+a + +b) }))}   
-        }  })
-
  
-/* ------------------Boton para Vaciar Carro------------------------- */
-btnVaciar.addEventListener('click' , () => {
-    let carrito = new Array();
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-    productos.forEach(product=>{
-        product.style.display="none"})
-})
-/* ----------------Boton para eliminar Producto------------------------------ */
-let items = JSON.parse(carrito);
-
-btnDelete.forEach(btn => {
-    btn.addEventListener('click' , (e) => {
-
-        if(carrito.includes(btn.getAttribute("target"))){
-        let ProductDelete = +btn.getAttribute("target")
+    productos.forEach((product)=>{
+        let price = Number(product.children[1].getAttribute("target"))
+        let select = product.children[2];
+        let Total = product.children[3];
+        Total.innerHTML= toThousand(price)
         
-        for (let i = 0; i < items.length; i++) {
-            if(items[i] == ProductDelete){
-            items.splice(i,1);
-        break;
-        }  
-    }
-}
-/* ------------------ACTUUALIZAR EL PRECIO FINAL--------------------- */
-let resta = btn.previousElementSibling.textContent.replace("." , "")
-let precioFinal = (AllPrices.textContent).replace("." , "")
-AllPrices.innerHTML= toThousand(Number(precioFinal-resta))
-console.log(items)
-console.log(resta)
-localStorage.setItem("carrito",JSON.stringify(items)) 
-btn.parentElement.style.display="none"
+/* --------------SI CAMBIA EL PRODUCTO------------------------ */
+        if(product.style.display !== "none"){
+        product.onchange = function() {
+        Total.innerHTML = toThousand(+select.value * price)
+        let carro=[]
+        
+        ProdPrice.forEach(t => {
+        JSON.parse(carrito).forEach(item =>{ 
+            if(item ==(t.getAttribute("target"))){
+                carro.push((t.textContent).replace("." , ""))    
+            }   })  })
+        AllPrices.innerHTML=toThousand(carro.reduce((a,b) => {return Number(+a + +b) }))
+            console.log(carro) 
+        }}       
+        })
 
-})
-     
-})
+    
+/* ------------------Boton para Vaciar Carro----------------------- */
+        btnVaciar.addEventListener('click' , () => {
+        let carrito = new Array();
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        productos.forEach(product=>{
+            product.style.display="none"})
+        })
+/* ----------------Boton para eliminar Producto--------------------- */
+        let items = JSON.parse(carrito);
+
+        btnDelete.forEach(btn => {
+        btn.addEventListener('click' , (e) => {
+            let ProductDelete = +btn.getAttribute("target")
+            
+            for (let i = 0; i < items.length; i++) {
+            if(items[i] == ProductDelete){
+                items.splice(i,1);
+            break;
+            }  
+        }
+
+/* ------------------ACTUUALIZAR EL PRECIO FINAL--------------------- */
+            let resta = btn.previousElementSibling.textContent.replace("." , "")
+            let precioFinal = (AllPrices.textContent).replace("." , "")
+            AllPrices.innerHTML= toThousand(Number(precioFinal-resta))
+            console.log(items)
+            console.log(resta)
+            localStorage.setItem("carrito",JSON.stringify(items)) 
+            btn.parentElement.style.display="none"
+
+            })
+        
+            })
 
 
 
