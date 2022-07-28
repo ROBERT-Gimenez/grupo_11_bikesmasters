e7ec9828ad0f9299e0fa6532b4963e5def84ccba
ROBERT-Gimenez <robertgim37@gmail.com>
2022-07-28 18:14:01 -0300

    iconos de register recuperados

--- public/js/registerValidation.js
+++ public/js/registerValidation.js
@@ -54,129 +54,131 @@ function checkChar(e) {
     }
 }
 
-/* const validInput = (icon)=>{
-    icon.classList.remove("input-error")
-    icon.classList.add("input-pass")
+ const validInput = (icon)=>{
+    icon.classList.remove("input_invalido")
+    icon.classList.add("valido")
     icon.classList.remove("fa-circle-exclamation","icon__invalid")
     icon.classList.add("fa-solid", "fa-circle-check","icon__valid")
-} */
+} 
 
-/* const invalidInput = (icon)=>{
-    icon.classList.remove("input-pass")
-    icon.classList.add("input-error")
+ const invalidInput = (icon)=>{
+    icon.classList.remove("valido")
+    icon.classList.add("input_invalido")
     icon.classList.remove("fa-circle-check","icon__valid")
     icon.classList.add("fa-solid","fa-circle-exclamation","icon__invalid")
     
-} */
+} 
 
-$name.addEventListener('blur' , function(e){
+$name.addEventListener('keyup' , function(e){
         switch (true) { 
             case !$name.value.trim()||$name.value.length < 4:
                 $nameError.innerHTML= "Se requiere un nombre valido"
                 $name.classList.add("invalido")
-/*                 invalidInput($name.nextElementSibling) */
+                invalidInput($name.nextElementSibling.children[0]) 
                 $name.nextElementSibling.classList.add("show__icon")
 
                 break;
             case !regExAlpha.test($name.value):
                 $nameError.innerHTML ="Nombre Invalido , no debe poseer numeros"
                 $name.classList.add("invalido");
-/*                 invalidInput($name.nextElementSibling) */
+                invalidInput($name.nextElementSibling) 
                 $name.nextElementSibling.classList.add("show__icon")
                 break;
             default :
                 $name.classList.remove("invalido");
-                $name.classList.add("valido");
-                $name.nextElementSibling.classList.remove("show__icon")
-/*                 validInput($name.nextElementSibling) */
-                $nameError.innerHTML =""
-                break;
+                $name.classList.add("valido");          
+                validInput($name.nextElementSibling.children[0]) 
+/*                 $name.nextElementSibling.classList.remove("show__icon")
+ */                $nameError.innerHTML =""
+                break; 
             }
 
 })
 /* >> validacion de Email << */
-$email.addEventListener("blur" , function(e) {
+$email.addEventListener("keyup" , function(e) {
     switch (true) { 
         case !$email.value.trim():
             $emailError.innerHTML= "Debe ingresar un email";
             $email.classList.add("invalido")
             $email.nextElementSibling.classList.add("show__icon")
-/*             invalidInput($email.nextElementSibling) */
+            invalidInput($email.nextElementSibling.children[0])
             break;
         case !regExEmail.test($email.value):
             $emailError.innerHTML ="Email Invalido"
             $email.classList.add("invalido");
             $email.nextElementSibling.classList.add("show__icon")
-/*             invalidInput($email.nextElementSibling) */
+            invalidInput($email.nextElementSibling.children[0])
             break;
         default :
             $email.classList.remove("invalido");
             $email.classList.add("valido");
             $emailError.innerHTML =""
-            $email.nextElementSibling.classList.remove("show__icon")
-/*             validInput($email.nextElementSibling) */
+            /* $email.nextElementSibling.classList.remove("show__icon") */
+            validInput( $email.nextElementSibling.children[0])
             break;
         }
 })
-$email.addEventListener('blur', () => {
+$email.addEventListener('keyup', () => {
     fetch('http://localhost:4000/api/Usuario')
     .then(response => response.json())
     .then(result => {
         result.data.forEach(element => {
             if($email.value === element.email){
                 $email.nextElementSibling.classList.add("show__icon")
-/*                 invalidInput($email.nextElementSibling) */
+                invalidInput($email.nextElementSibling.children[0])
                 $emailError.innerHTML = "El email ya se encuentra registrado";
                 $email.classList.add('invalido');
             }
+               
+            
         });
     })
 })
 /* >> validacion de Password << */
-$password.addEventListener('blur', function(){
+$password.addEventListener('keyup', function(){
     switch (true) {
         case !$password.value.trim():
             $passwordError.innerHTML = 'Coloque una contraseña'
             $password.classList.add('invalido')
             $password.nextElementSibling.classList.add("show__icon")
-/*             invalidInput($password.nextElementSibling) */
+            invalidInput($password.nextElementSibling.children[0])
             break;
         case !regExPass.test($password.value):
             $passwordError.innerHTML = 'La contraseña debe tener: entre 8 y 12 caracteres, al menos una mayúscula, una minúscula y un número';
             $password.classList.add('invalido')
             $password.nextElementSibling.classList.add("show__icon")
-/*             invalidInput($password.nextElementSibling) */
+            invalidInput($password.nextElementSibling.children[0])
             break;    
         default:
             $password.classList.remove("invalido");
             $password.classList.add('valido')
-            $password.nextElementSibling.classList.remove("show__icon")
-/*             validInput($password.nextElementSibling) */
+            /* $password.nextElementSibling.classList.remove("show__icon") */
+            validInput($password.nextElementSibling.children[0])
             $passwordError.innerHTML = ""
             break;
     }
 })
 /* >> validacion de Password2 << */
-$password2.addEventListener('blur', function(){
+$password2.addEventListener('keyup', function(){
     switch (true) {
         case $password2.value.length == "":
             $password2Error.innerHTML = 'Reingresa tu contraseña'
             $password2.classList.add('invalido')
             $password2.nextElementSibling.classList.add("show__icon")
-/*             invalidInput($password2.nextElementSibling)
- */            break;
+            invalidInput($password2.nextElementSibling.children[0])
+            break;
         case $password2.value !== $password.value:
             $password2Error.innerHTML = 'Las contraseñas no coinciden';
             $password2.classList.add('invalido')
             $password2.nextElementSibling.classList.add("show__icon")
-            /* invalidInput($password2.nextElementSibling) */
+            invalidInput($password2.nextElementSibling.children[0])
             break;    
         default:
             $password2.classList.remove("invalido");
             $password2.classList.add('valido')
-            $password2.nextElementSibling.classList.remove("show__icon")
-/*             validInput($password2.nextElementSibling)
- */         $password2Error.innerHTML = ""
+            /* $password2.nextElementSibling.classList.remove("show__icon") */
+            validInput($password2.nextElementSibling.children[0])
+            $password2Error.innerHTML = ""
             break;
     }
 })
