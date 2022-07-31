12d8c04e80a9444dbeaac6445e8a3e2902611256
Brian Flores <brian.flores.ch@gmail.com>
2022-07-30 21:37:46 -0300

    Agrega iconos de medios de pago

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 
+router.get('/nuestros-productos', productController.allProducts)
 /*** GET ONE PRODUCT ***/ 
 router.get('/detalle/:id', productController.detalle); 
 router.get('/categoria/:id', productController.Category); 
