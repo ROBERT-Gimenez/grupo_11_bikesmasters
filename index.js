190fda62277f521b00ec2b10ddafc9bd69f5e879
ROBERT-Gimenez <robertgim37@gmail.com>
2022-08-18 23:51:40 -0300

    conf

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
+const bike = require('./src/app')
 const app = express();
 
 const PORT =process.env.PORT || 4000;
-
+app.use( '/' , bike)
 app.listen(PORT , () => console.log(`puesto levantado en ${PORT}`))
